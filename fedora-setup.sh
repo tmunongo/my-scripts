d86df0dc4ff6e0c580aa60ff9dfdf51e6a073f10
Tawanda Munongo <ejmunongo@gmail.com>
2023-04-22 08:59:35 +0200

    updates to the shell script

--- fedora-setup.sh
+++ fedora-setup.sh
@@ -4,25 +4,27 @@
 sudo dnf update -y
 sudo dnf upgrade -y
 # add rpm fusion
-sudo dnf install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm
-sudo dnf install https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+sudo dnf -y install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm
+sudo dnf -y install https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
 
 sudo dnf upgrade --refresh
-sudo dnf install gnome-tweak-tool
 # enable Flathub
+sudo dnf -y install flatpak
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 sudo dnf groupupdate core
 # software installation
-sudo dnf -y chromium
+sudo flatpak install -y flathub com.spotify.Client
+sudo dnf -y chromium flameshot
 sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
 sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
+dnf check-update
 sudo dnf -y install code
 sudo dnf -y module reset nodejs
 sudo dnf -y install nodejs
 
 # docker
 sudo dnf -y install dnf-plugins-core
-sudo dnf config-manager \
+sudo dnf -y config-manager \
     --add-repo \
     https://download.docker.com/linux/fedora/docker-ce.repo
 sudo dnf -y install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
@@ -37,19 +39,33 @@ sudo dnf -y install kernel-headers kernel-devel dkms elfutils-libelf-devel qt5-q
 cat <<EOF | sudo tee /etc/yum.repos.d/virtualbox.repo 
 [virtualbox]
 name=Fedora $releasever - $basearch - VirtualBox
-baseurl=http://download.virtualbox.org/virtualbox/rpm/fedora/36/\$basearch
+baseurl=http://download.virtualbox.org/virtualbox/rpm/fedora/\$releasever/\$basearch
 enabled=1
 gpgcheck=1
 repo_gpgcheck=1
 gpgkey=https://www.virtualbox.org/download/oracle_vbox.asc
 EOF
 sudo dnf search virtualbox
-sudo dnf install VirtualBox-7.0
+sudo dnf -y install VirtualBox-7.0
+sudo usermod -a -G vboxusers $USER
+newgrp vboxusers
 
-sudo dnf -y install vlc neofetch skype vagrant
+# skype
+sudo curl -o /etc/yum.repos.d/skype-stable.repo https://repo.skype.com/rpm/stable/skype-stable.repo
+sudo dnf -y install skypeforlinux
+
+sudo dnf -y install vlc neofetch vagrant ansible
 
 # Remove Libre Office and install WPS Office
 sudo dnf -y remove libreoffice*
 sudo dnf -y install https://linux.mirrors.es.net/wps-office/11/x86_64/wps-office-11.1.0.10740-1.x86_64.rpm
 
 sudo dnf -y install tlp tlp-rdw
+
+# zsh and oh my zsh
+sudo dnf -y install zsh
+sudo lchsh /bin/zsh $USER
+
+if [ -x "S(command -v zsh)" ]; then
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+fi
