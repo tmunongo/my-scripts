fac6a37e280d0fd4679d15a3fca2c871d0ca8763
Tawanda Munongo <ejmunongo@gmail.com>
2023-08-28 15:53:19 +0200

    updated opensuse script

--- openSUSE-setup.sh
+++ openSUSE-setup.sh
@@ -9,34 +9,38 @@ sudo zypper update
 sudo zypper ar -f -n rpmfusion-free http://download1.rpmfusion.org/free/opensuse/leap/<release>/rpmfusion-free-release.rpm
 sudo zypper ar -f -n rpmfusion-nonfree http://download1.rpmfusion.org/nonfree/opensuse/leap/<release>/rpmfusion-nonfree-release.rpm
 
+# Skype
+sudo zypper addrepo https://repo.skype.com/rpm/stable/skype-stable.repo
+sudo zypper update
+sudo zypper install -y skypeforlinux
+
 # Enable Flathub
 sudo zypper install -y flatpak
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 
-# Install software
-sudo flatpak install -y flathub com.spotify.Client
-sudo zypper install -y chromium flameshot
-sudo zypper addrepo -f https://packages.microsoft.com/yumrepos/vscode vscode
-sudo zypper install -y code
-sudo zypper install -y nodejs
+sudo zypper install -y chromium flameshot vivaldi flameshot-fish-completion flameshot-zsh-completion intellij android-studio
+# VS Code
+sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
+sudo zypper addrepo https://packages.microsoft.com/yumrepos/vscode vscode
+sudo zypper refresh
+sudo zypper install -y code nodejs vagrant
 
 # Docker
-sudo zypper install -y docker
-sudo systemctl start docker
-sudo usermod -aG docker $USER
+zypper install -y docker
+systemctl start docker
+usermod -aG docker $USER
 
 # VirtualBox
 sudo zypper install -y virtualbox
 
-# Skype
-sudo zypper ar -cfp 90 https://repo.skype.com/rpm/stable/skype-stable.repo
-sudo zypper install -y skypeforlinux
-
-# VLC, Neofetch, Vagrant, Ansible, Neovim
-sudo zypper install -y vlc neofetch vagrant neovim python3-neovim
+# VLC, Neofetch, Vagrant, Ansible, Deluge, Neovim
+sudo zypper install -y vlc deluge neofetch vagrant neovim python3-neovim
 
 # TLP
 sudo zypper install -y tlp tlp-rdw
+
+# Install software
+sudo flatpak install -y flathub com.spotify.Client md.obsidian.Obsidian Postman Jetbrains.IntelliJ-IDEA-Community
 
 # Zsh and Oh My Zsh
 sudo zypper install -y zsh
